d1ccbc4b96041ba7273728c751d560668af1fcf1
KrasniKot <emanueltrias9@gmail.com>
2024-08-02 22:17:03 +0000

    common new version

--- models/common.py
+++ models/common.py
@@ -2,7 +2,8 @@
     that defines all the attributes and methods
     all classes must have
 
-    requires: no installation of external library
+Created by:
+    Emanuel Trias
 """
 
 from abc import ABC, abstractmethod
@@ -11,24 +12,36 @@ import json
 import uuid
 
 from server.v1.utils.version_manager import VersionManager
+vm = VersionManager()
 
 
 class Common(ABC):
     """ Defines a Common class """
-    version = '0.1.0'
+    version = '0.4.3'
+
+    def __init_subclass__(cls, **kwargs):
+        """ Defines action to be performed always upon initialization of subclasses """
+        vm.save_version(cls.__name__, cls.version)
 
     def __init__(self):
         """ Initializes a Common class """
-        self.__oid = uuid.uuid4()
-        self.__created_at = datetime.utcnow()
-        self.__updated_at = datetime.utcnow()
+        self.oid = uuid.uuid4()
+        self.public_oid = "Not yet implemented"
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        self.__version_saved = False
 
-        self.save_version('Common', Common.version)
+        vm.save_version('Common', Common.version)
 
     @property
     def oid(self):
         """ oid getter method """
         return self.__oid
+
+    @property
+    def public_oid(self):
+        """ public oid getter method """
+        return self.__public_oid
 
     @property
     def created_at(self):
@@ -40,6 +53,11 @@ class Common(ABC):
         """ updated_at getter method """
         return self.__updated_at
 
+    @property
+    def version_saved(self):
+        """ updated_at getter method """
+        return self.__version_saved
+
     @oid.setter
     def oid(self, newid):
         """ oid setter method """
@@ -48,6 +66,11 @@ class Common(ABC):
 
         self.__oid = newid
 
+    @public_oid.setter
+    def public_oid(self, newid):
+        """ public oid setter method """
+        self.__public_oid = newid
+
     @created_at.setter
     def created_at(self, newca):
         """ created_at setter method """
@@ -55,6 +78,13 @@ class Common(ABC):
             raise TypeError(f"New created_at must be of type datetime, not {type(newca)}")
 
         self.__created_at = newca
+
+    @updated_at.setter
+    def updated_at(self, newaa):
+        """ updated_at setter method """
+        if not isinstance(newaa, datetime):
+            raise TypeError(f"New updated_at must be of type datetime, not {type(newaa)}")
+        self.__updated_at = newaa
 
     @abstractmethod
     def to_dict(self):
@@ -65,15 +95,15 @@ class Common(ABC):
             - name: name of the attribute to be updated
             - value: value to be set
         """
-        if name != '_Common__updated_at':  # Check the correct mangled name
+        if name != '_Common__updated_at':
             super().__setattr__('_Common__updated_at', datetime.utcnow())
         super().__setattr__(name, value)
 
-    def save_version(self, c, v):
-        with open('./server/versions.json', 'r', encoding='utf-8') as f:
-            data = json.load(f)
-
-        data[c] = v
-
-        with open('./server/versions.json', 'w', encoding='utf-8') as f:
-            data = json.dump(data, f, indent=4)      
+    def save_version(self, classname, classversion):
+        """ Saves a the version of a class
+            - classname
+            - classversion
+        """
+        if not self.__version_saved:
+            vm.save_version(classname, classversion)
+            self.__version_saved = True
