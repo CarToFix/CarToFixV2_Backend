33bd3dd7af6c898fd93e8fa9d7d4c50d465a8dc4
LeoRod17 <matiassotelo171097@gmail.com>
2024-11-06 12:06:28 -0800

    adding new branch and the title of the first function

--- server/v1/views/workshops.py
+++ server/v1/views/workshops.py
@@ -13,13 +13,20 @@ class CreateWorkshop(BaseModel):
     name: str
     tel: str
 
+
 @route.post("/workshop", status_code=201)
 async def create_workshop(client_request: CreateWorkshop):
     """Create and saves the workshop"""
-    workshop = Workshop(mail=client_request.mail, name=client_request.name, tel=client_request.tel)
+    workshop = Workshop(mail=client_request.mail,
+                        name=client_request.name, tel=client_request.tel)
 
     workshop.save()
 
     wdict = workshop.to_dict({'show': ['mail', 'name', 'tel', 'id']})
     print(wdict)
     return {"message": "the workshop has been created successfully"} | wdict
+
+
+@route.get("/workshop<workshop_id>", status_code=201)
+async def search_workshop(workshop):
+    """a function to search a workshop"""
